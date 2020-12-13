27692d15a4e7d429ace0fae5045589f0c5aeae58
Kohei107 <k.s107bb@gmail.com>
2020-12-13 18:29:37 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ToDoリスト</title>
+</head>
+<body>
+<h1>ToDoリスト</h1>
+  <form action="index.php" method="post">
+
+    <ul>
+      <li>
+      <span>タスク名</span><input type="text" name="name">
+      </li>
+      <li>
+      <input type="submit" name="submit">
+      </li>
+    </ul>
+
+  </form>
+</body>
+</html>
