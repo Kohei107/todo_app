67af4b84b7b48b634005ab567d5fef82b506bd82
Kohei107 <k.s107bb@gmail.com>
2020-12-13 18:33:22 +0900

    テーブル紐付け

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<?php
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+
+if(isset($_POST['submit'])){
+
+  $name = $_POST['name'];
+  $name = htmlspecialchars($name, ENT_QUOTES);
+
+    
+
+  $dsn = 'mysql:dbname=todolist;host=localhost;charset=utf8';
+  $user = 'root';
+  $password = 'root';
+
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+  $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+  $sql = 'INSERT INTO tasks (name, done) VALUES (?, 0)';
+  $stmt = $dbh->prepare($sql);
+
+  $stmt->bindValue(1, $name, PDO::PARAM_STR);
+
+  $stmt->execute();
+
+  $dbh = null;
+
+  unset($name);
+
+
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
