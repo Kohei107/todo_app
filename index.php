bd3cb7836c7103698e62e8656a5c4e8143994672
Kohei107 <k.s107bb@gmail.com>
2020-12-13 18:41:37 +0900

    エラー表示

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
-ini_set("display_errors", 1);
-error_reporting(E_ALL);
-
 
 if(isset($_POST['submit'])){
 
   $name = $_POST['name'];
   $name = htmlspecialchars($name, ENT_QUOTES);
 
-    
+    try{
 
   $dsn = 'mysql:dbname=todolist;host=localhost;charset=utf8';
   $user = 'root';
@@ -17,6 +14,12 @@ if(isset($_POST['submit'])){
   $dbh = new PDO($dsn, $user, $password);
   $dbh->query('SET NAMES utf8');
   $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    } catch (PDOException $e) {
+        print "エラー: " . $e->getMessage() . "<br/>";
+
+        die();
+      }
 
   $sql = 'INSERT INTO tasks (name, done) VALUES (?, 0)';
   $stmt = $dbh->prepare($sql);
